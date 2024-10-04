31b60e34e35d7eb7d1919840d1ce7f0aaa77cefd
SIA <i.saenko@estesis.tech>
2024-10-04 14:31:31 +0700

    backup

--- src/pages/AnalyticsPage.vue
+++ src/pages/AnalyticsPage.vue
@@ -1,66 +1,32 @@
-<script setup lang="ts" >
-import { ref } from 'vue'
+<template>
+  <BasePage>
+    <n-card class="grid-container">
+      <SelectorsMenu />
+      <TableBlock />
+    </n-card>
+  </BasePage>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
 import { NCard } from 'naive-ui';
 import BasePage from '../components/BasePage.vue';
 import SelectorsMenu from '../components/AnalyticsPage/SelectorsMenu.vue';
 import TableBlock from '../components/AnalyticsPage/TableBlock.vue';
 
-const data = ref( [
-  {
-    group: "Все муниципалитеты",
-    participants: 2304,
-    grade2: "5.1",
-    grade3: "41.9",
-    grade4: "48",
-    grade5: "37",
-    averageGrade: 3.6,
-    educationQuality: 53.1,
-    successRate: 95
+export default defineComponent({
+  components: {
+    NCard,
+    BasePage,
+    SelectorsMenu,
+    TableBlock
   },
-  {
-    group: "Алейский район",
-    participants: 14,
-    grade2: "5.1",
-    grade3: "41.9",
-    grade4: "48",
-    grade5: "37",
-    averageGrade: 3.5,
-    educationQuality: 50,
-    successRate: 85.7
-  },
-  {
-    group: "МКУ «Кашинская средняя общеобразовательная школа» имени Героя России А.И. Сугакова",
-    participants: 8,
-    grade2: "5.1",
-    grade3: "41.9",
-    grade4: "48",
-    grade5: "37",
-    averageGrade: 3.9,
-    educationQuality: 75,
-    successRate: 87.5
-  },
-  {
-    group: "МКУ \"Уропинская СОШ\"",
-    participants: 6,
-    grade2: "5.1",
-    grade3: "41.9",
-    grade4: "48",
-    grade5: "37",
-    averageGrade: 3,
-    educationQuality: 16.7,
-    successRate: 83.4
+  setup() {
+    return {
+      }
   }
-]);
+});
 </script>
-
-<template>
-  <BasePage>
-    <n-card class="grid-container">
-      <SelectorsMenu />
-      <TableBlock :data="data"  />
-    </n-card>
-  </BasePage>
-</template>
 
 <style scoped>
 .n-card {
